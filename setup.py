f8bb54631df8f14e571c472a07eca418ab36cd54
Anurag Raj <181159358+Virtuoso-8051@users.noreply.github.com>
2026-06-28 13:04:13 +0530

    exception & logging completed

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 ''' setup.py file is essential for packaging and distributing Python projects. It contains metadata about the project, such as its name, version, author, and dependencies. The setup.py file is used by tools like pip to install the package and its dependencies. It can also include additional information like entry points for command-line scripts, classifiers for categorizing the project, and other configuration options. Overall, setup.py is a crucial component for managing and sharing Python projects effectively. '''
-# setup file is essential part of packaging
-
 
 from setuptools import find_packages,setup
 from typing import List
