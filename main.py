f032ffd8237409ddd4b46ef55ddeda2a25f3c19d
Anurag Raj <181159358+Virtuoso-8051@users.noreply.github.com>
2026-09-26 00:37:00 +0530

    step4: model training done & mlflow implemented

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
-# from networksecurity.components.model_trainer import ModelTrainer
-# from networksecurity.entity.config_entity import ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
  
 import sys
 
@@ -33,11 +33,11 @@ if __name__=='__main__':
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
 
-        # logging.info("Model Training sstared")
-        # model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
-        # model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
-        # model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")   
 
-        # logging.info("Model Training artifact created")    
     except Exception as e:
            raise NetworkSecurityException(e,sys)
