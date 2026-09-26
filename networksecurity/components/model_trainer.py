7073ac731a580ae1df8487a077790594aad1c6ef
Anurag Raj <181159358+Virtuoso-8051@users.noreply.github.com>
2026-09-26 16:39:13 +0530

    connecting dagshub & pushing model to final_model.py

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -20,13 +20,13 @@ from sklearn.ensemble import (
     AdaBoostClassifier,
     GradientBoostingClassifier,
     RandomForestClassifier,
-)
+) 
 
 import mlflow
 
 # from urllib.parse import urlparse
-# import dagshub
-# #dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+import dagshub
+dagshub.init(repo_owner='rajanurag1508', repo_name='Network-Security', mlflow=True)
 # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
 # os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
 # os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
@@ -127,6 +127,7 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
+        
         #model pusher
         save_object("final_model/model.pkl",best_model)
         
